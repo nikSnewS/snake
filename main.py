37833fa056b9b2c404b637c7c8154969d6a11805
nikSnewS <74721227+nikSnewS@users.noreply.github.com>
2020-12-17 11:03:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,93 +1,154 @@
 import pygame
-import sys
+from random import randrange
 
+RES = 700
+SIZE = 50
 
-def check_win(mas, sign):
-    zeroes = 0
-    for row in mas:
-        zeroes += row.count(0)
-        if row.count(sign) == 3:
-            return sign
-    for col in range(1, 2): # 0, 1, 2
-        if mas[0][col] == sign and mas[1][col] == sign and mas[2][col] == sign:
-            return sign
-    if mas[0][0] == sign and mas[1][1] == sign and mas[2][2] == sign:
-        return sign
-    if mas[0][2] == sign and mas[1][1] == sign and mas[2][0] == sign:
-        return sign
-    if zeroes == 0:
-        return 'Piece'
-    return False
-
+x, y = randrange(SIZE, RES - SIZE, SIZE), randrange(SIZE, RES - SIZE, SIZE)
+apple = randrange(SIZE, RES - SIZE, SIZE), randrange(SIZE, RES - SIZE, SIZE)
+length = 1
+snake = [(x, y)]
+dx, dy = 0, 0
+fps = 60
+dirs = {'W': True, 'S': True, 'A': True, 'D': True, }
+score = 0
+speed_count, snake_speed = 0, 10
+slize = (0, 0)
+game_over = False
+stone = (randrange(SIZE, RES - SIZE, SIZE), randrange(SIZE, RES - SIZE, SIZE), randrange)
 
 pygame.init()
-block_size = 150
-margin = 10
-wigth = height = block_size * 4 + margin * 5
+surface = pygame.display.set_mode([RES, RES])
+clock = pygame.time.Clock()
+font_score = pygame.font.SysFont('Arial', 26, bold=True)
+font_end = pygame.font.SysFont('Arial', 66, bold=True)
 
-win_size = (wigth, height)
-screen = pygame.display.set_mode(win_size)
-pygame.display.set_caption("Крестики нолики")
+img = pygame.image.load('1.jpg').convert_alpha()
+img_apple = pygame.image.load('apple.png').convert_alpha()
+img_face = pygame.image.load('snakeface.png').convert_alpha()
+img_body = pygame.image.load('body.png').convert_alpha()
+img_tail = pygame.image.load('tail.png').convert_alpha()
+img_slize = pygame.image.load('slize.png').convert_alpha()
+img_stone = pygame.image.load('stone.png').convert_alpha()
 
-pink = (250, 5, 34)
-green = (0, 255, 0)
-white = (255, 255, 255)
-red = (130, 9, 21)
-black = (0, 0, 0)
-mas = [[0] * 4 for i in range(4)]
-query = 0  # 1 2 3 4 5 6 7
-game_over = False
 
-while True:
+def close_game():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit((0))
-        elif event.type == pygame.MOUSEBUTTONDOWN and not game_over:
-            x_mouse, y_mouse = pygame.mouse.get_pos()
-            col = x_mouse // (block_size + margin)
-            row = y_mouse // (block_size + margin)
-            if mas[row][col] == 0:
-                if query % 2 == (0):
-                    mas[row][col] = 'победил X'
-                else:
-                    mas[row][col] = 'Победил O'
-                query += 1
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            game_over = False
-            mas = [[0] * 4 for i in range(4)]
-            query=0
-            screen.fill(black)
+            exit()
 
-    if not game_over:
-        for row in range(4):
-            for col in range(4):
-                if mas[row][col] == 'победил X':
-                    color = red
-                elif mas[row][col] == 'Победил O':
-                    color = green
-                else:
-                    color = white
-                x = col * block_size + (col + 1) * margin
-                y = row * block_size + (row + 1) * margin
-                pygame.draw.rect(screen, color, (x, y, block_size, block_size))
-                if color == red:
-                    pygame.draw.line(screen, white, (x + 5, y + 5), (x + block_size - 5, y + block_size - 5), 3)
-                    pygame.draw.line(screen, white, (x + block_size - 5, y + 5), (x + 5, y + block_size - 5), 3)
-                elif color == green:
-                    pygame.draw.circle(screen, white, (x + block_size // 2, y + block_size // 2), block_size // 2 - 3, 3)
-        if (query - 1) % 2 == 0:  # x
-            game_over = check_win(mas, 'победил X')
-        else:
-            game_over = check_win(mas, 'Победил O')
+while True:
+    if game_over:
+        render_end = font_end.render('GAME OVER', 1, pygame.Color('orange'))
+        surface.blit(render_end, (RES // 2 - 200, RES // 3))
+        pygame.display.flip()
+        close_game()
+    else:
+        surface.fill('black', (0,0, RES, RES))
+        surface.blit(img, (0, 0))
+        # drawing snake, apple
+        # [pygame.draw.rect(surface, pygame.Color('green'), (i, j, SIZE - 1, SIZE - 1)) for i, j in snake[1:-1]]
+        [surface.blit(img_body, (i,j)) for i, j in snake[1:-1]]
+        surface.blit(img_tail, snake[0])
+        surface.blit(img_face, snake[-1])
+        # pygame.draw.rect(surface, pygame.Color('red'), (*apple, SIZE, SIZE))
+        surface.blit(img_apple, apple)
+        # show score
+        render_score = font_score.render(f'SCORE: {score}', 1, pygame.Color('orange'))
+        surface.blit(render_score, (5, 5))
+        # snake movement
+        speed_count += 1
+        if not speed_count % snake_speed:
+            x += dx * SIZE
+            y += dy * SIZE
+            if x < 0: x = RES - SIZE
+            if x > RES - SIZE: x = 0
+            if y < 0: y = RES - SIZE
+            if y > RES - SIZE: y = 0
+            snake.append((x, y))
+            snake = snake[-length:]
+    # eating food
+    if snake[-1] == apple:
+        apple = randrange(SIZE, RES - SIZE, SIZE), randrange(SIZE, RES - SIZE, SIZE)
+        while apple in snake:
+            apple = randrange(SIZE, RES - SIZE, SIZE), randrange(SIZE, RES - SIZE, SIZE)
+        length += 1
+        score += 1
+        snake_speed -= 1
+        snake_speed = max(snake_speed, 10)
+    # stone
+        # pygame.draw.rect(surface, pygame.Color('grey'), (*stone, SIZE, SIZE))
+        surface.blit(img_stone, stone)
+    # attack
 
-        if game_over:
-            screen.fill(black)
-            font = pygame.font.SysFont('stxingkai', 80)
-            text1 = font.render(game_over, True, white)
-            text_rect = text1.get_rect()
-            text_x = screen.get_width() / 2 - text_rect.width / 2
-            text_y = screen.get_width() / 2 - text_rect.height / 2
-            screen.blit(text1, [text_x, text_y])
 
-    pygame.display.update()
+    # game over
+    if len(snake) != len(set(snake)) or (x >= stone[0] and x <= stone[0]+ stone[2]*SIZE and y >= stone[1] and y <= stone[1]+stone[2]*SIZE):
+        game_over = True
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_RETURN] and game_over:
+        game_over = False
+        x, y = randrange(SIZE, RES - SIZE, SIZE), randrange(SIZE, RES - SIZE, SIZE)
+        apple = randrange(SIZE, RES - SIZE, SIZE), randrange(SIZE, RES - SIZE, SIZE)
+        length = 1
+        snake = [(x, y)]
+        dx, dy = 0, 0
+        dirs = {'W': True, 'S': True, 'A': True, 'D': True, }
+        score = 0
+        speed_count, snake_speed = 0, 10
+
+    pygame.display.flip()
+    clock.tick(fps)
+    close_game()
+    # controls
+
+
+    if key[pygame.K_w]:
+        if dirs['W']:
+            if dx == 1 and dy == 0:
+                img_face = pygame.transform.rotate(img_face, -90)
+            elif dx == -1 and dy == 0:
+                img_face = pygame.transform.rotate(img_face, 90)
+            if dx == 1 and dy == 0:
+                img_tail = pygame.transform.rotate(img_tail, -90)
+            elif dx == -1 and dy == 0:
+                img_tail = pygame.transform.rotate(img_tail, 90)
+            dx, dy = 0, -1
+            dirs = {'W': True, 'S': False, 'A': True, 'D': True}
+    elif key[pygame.K_s]:
+        if dirs['S']:
+            if dx == 1 and dy == 0:
+                img_face = pygame.transform.rotate(img_face, 90)
+            elif dx == -1 and dy == 0:
+                img_face = pygame.transform.rotate(img_face, -90)
+            if dx == 1 and dy == 0:
+                img_tail = pygame.transform.rotate(img_tail, -90)
+            elif dx == -1 and dy == 0:
+                img_tail = pygame.transform.rotate(img_tail, 90)
+            dx, dy = 0, 1
+            dirs = {'W': False, 'S': True, 'A': True, 'D': True}
+    elif key[pygame.K_a]:
+        if dirs['A']:
+            if dx == 0 and dy == -1:
+                img_face = pygame.transform.rotate(img_face, 90)
+            elif dx == 0 and dy == 1:
+                img_face = pygame.transform.rotate(img_face, -90)
+            if dx == 1 and dy == 0:
+                img_tail = pygame.transform.rotate(img_tail, -90)
+            elif dx == -1 and dy == 0:
+                img_tail = pygame.transform.rotate(img_tail, 90)
+            dx, dy = -1, 0
+            dirs = {'W': True, 'S': True, 'A': True, 'D': False}
+    elif key[pygame.K_d]:
+        if dirs['D']:
+            if dx == 0 and dy == -1:
+                img_face = pygame.transform.rotate(img_face, -90)
+            elif dx == 0 and dy == 1:
+                img_face = pygame.transform.rotate(img_face, 90)
+            if dx == 1 and dy == 0:
+                img_tail = pygame.transform.rotate(img_tail, -90)
+            elif dx == -1 and dy == 0:
+                img_tail = pygame.transform.rotate(img_tail, 90)
+            dx, dy = 1, 0
+            dirs = {'W': True, 'S': True, 'A': False, 'D': True}
